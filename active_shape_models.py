8c4e0e40dd799cb61289ec2869286339e4304e93
Andrew Chambers <andrewrchambers@googlemail.com>
2012-10-26 22:07:59 +0100

    Day two:
    * Shapes now align correctly to a mean shape
    * Can generate model - modes of variation show expected characteristics of the input datasets

--- active_shape_models.py
+++ active_shape_models.py
@@ -20,6 +20,15 @@ class Point ( object ):
     """
     return Point(self.x + p.x, self.y + p.y)
 
+  def __div__(self, i):
+    return Point(self.x/i, self.y/i)
+
+  def __eq__(self, other):
+    return self.x == other.x and self.y == other.y
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
   def __repr__(self):
     """return a string representation of this point. """
     return '(%f, %f)' % (self.x, self.y)
@@ -39,29 +48,55 @@ class Shape ( object ):
     self.pts = pts
     self.num_pts = len(pts)
 
+  def __add__(self, other):
+    """ Operator overloading so that we can add one shape to another
+    """
+    s = Shape([])
+    for i,p in enumerate(self.pts):
+      s.add_point(p + other.pts[i])
+    return s
+
+  def __div__(self, i):
+    """ Division by a constant.
+    Each point gets divided by i
+    """
+    s = Shape([])
+    for p in self.pts:
+      s.add_point(p/i)
+    return s
+
+  def __eq__(self, other):
+    for i in range(len(self.pts)):
+      if self.pts[i] != other.pts[i]:
+        return False
+    return True
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
   def add_point(self, p):
     self.pts.append(p)
     self.num_pts += 1
 
   """ Helper methods for shape alignment """
   def __get_X(self, w):
-    return sum([w[i]*pt.x for i,pt in enumerate(self.pts)])
+    return sum([w[i]*self.pts[i].x for i in range(len(self.pts))])
   def __get_Y(self, w):
-    return sum([w[i]*pt.y for i,pt in enumerate(self.pts)])
+    return sum([w[i]*self.pts[i].y for i in range(len(self.pts))])
   def __get_Z(self, w):
-    return sum([w[i]*(pt.x**2+pt.y**2) for i,pt in enumerate(self.pts)])
+    return sum([w[i]*(self.pts[i].x**2+self.pts[i].y**2) for i in range(len(self.pts))])
   def __get_C1(self, w, s):
-    return sum([w[i]*(pt.x*self.pts[i].x+pt.y*self.pts[i].y) for \
-        i,pt in enumerate(s.pts)])
+    return sum([w[i]*(s.pts[i].x*self.pts[i].x + s.pts[i].y*self.pts[i].y) \
+        for i in range(len(self.pts))])
   def __get_C2(self, w, s):
-    return sum([w[i]*(pt.y*self.pts[i].x-pt.x*self.pts[i].y) for \
-        i,pt in enumerate(s.pts)])
+    return sum([w[i]*(s.pts[i].y*self.pts[i].x - s.pts[i].x*self.pts[i].y) \
+        for i in range(len(self.pts))])
 
-  def align_to_shape(self, s, w):
-    """ Aligns this shape to the given shape using the weight matrix
-    w.  This applies a scaling, transformation and rotation to
-    each point in the shape to align it as closely as possible to the
-    shape.
+  def get_alignment_params(self, s, w):
+    """ Gets the parameters required to align the shape to the given shape
+    using the weight matrix w.  This applies a scaling, transformation and
+    rotation to each point in the shape to align it as closely as possible
+    to the shape.
 
     This relies on some linear algebra which we use numpy to solve.
 
@@ -74,7 +109,7 @@ class Shape ( object ):
 
     :param shape: The shape to align to
     :param w: The weight matrix
-    :return s: The aligned shape
+    :return x: [ax, ay, tx, ty]
     """
 
     X1 = s.__get_X(w)
@@ -86,27 +121,41 @@ class Shape ( object ):
     C1 = self.__get_C1(w, s)
     C2 = self.__get_C2(w, s)
 
-    a = np.array([[ X2, -Y2,  W,  0],
-                  [ Y2,  X2,  0,  W],
-                  [  Z,   0, X2, Y2],
-                  [  0,   Z,-Y2, X2]])
+    a = np.array([[ X2, -Y2,   W,  0],
+                  [ Y2,  X2,   0,  W],
+                  [  Z,   0,  X2, Y2],
+                  [  0,   Z, -Y2, X2]])
 
     b = np.array([X1, Y1, C1, C2])
     # Solve equations
     # result is [ax, ay, tx, ty]
-    res = np.linalg.solve(a, b)
-
-    print res
-
-    new = Shape()
+    return np.linalg.solve(a, b)
+    #new = Shape([])
+    #print res
+    # For each point in current shape
+    #for pt in self.pts:
+     # new_x = (res[0]*pt.x - res[1]*pt.y) + res[2]
+     # new_y = (res[1]*pt.x + res[0]*pt.y) + res[3]
+     # new.add_point(Point(new_x, new_y))
+    #eturn new
+  def apply_params_to_shape(self, p):
+    new = Shape([])
     # For each point in current shape
     for pt in self.pts:
-      new_x = (res[0]*pt.x - res[1]*pt.y) - res[2]
-      new_y = (res[1]*pt.x + res[0]*pt.y) - res[3]
+      new_x = (p[0]*pt.x - p[1]*pt.y) + p[2]
+      new_y = (p[1]*pt.x + p[0]*pt.y) + p[3]
       new.add_point(Point(new_x, new_y))
     return new
 
-    # Now apply scaling/rotation/translation to the shape
+  def align_to_shape(self, s, w):
+    p = self.get_alignment_params(s, w)
+    return self.apply_params_to_shape(p)
+
+  def get_vector(self):
+    vec = np.zeros((self.num_pts, 2))
+    for i in range(len(self.pts)):
+      vec[i,:] = [self.pts[i].x, self.pts[i].y]
+    return vec.flatten()
 
 class ShapeViewer ( object ):
   """ Provides functionality to display a shape in a window
@@ -119,16 +168,25 @@ class ShapeViewer ( object ):
     # Get size for the window
     max_x = int(max([pt.x for shape in shapes for pt in shape.pts]))
     max_y = int(max([pt.y for shape in shapes for pt in shape.pts]))
+    min_x = int(min([pt.x for shape in shapes for pt in shape.pts]))
+    min_y = int(min([pt.y for shape in shapes for pt in shape.pts]))
 
-    i = cv.CreateImage((max_x+20, max_y+20), cv.IPL_DEPTH_8U, 3)
+    i = cv.CreateImage((max_x-min_x+20, max_y-min_y+20), cv.IPL_DEPTH_8U, 3)
     cv.Set(i, 0)
     for shape in shapes:
-      r = randint(0, 255)
-      g = randint(0, 255)
-      b = randint(0, 255)
-      print "(%d, %d, %d)" % (r, g, b)
-      for pt in shape.pts:
-        cv.Circle(i, (int(pt.x), int(pt.y)), 1, (r, g, b))
+      #r = randint(0, 255)
+      #g = randint(0, 255)
+      #b = randint(0, 255)
+      r = 255
+      g = 255
+      b = 255
+      #print "(%d, %d, %d)" % (r, g, b)
+      curr_pt = shape.pts[0]
+      cv.Circle(i, (int(curr_pt.x-min_x), int(curr_pt.y-min_y)), 1, (r, g, b))
+      for pt in shape.pts[1:]:
+        cv.Circle(i, (int(pt.x-min_x), int(pt.y-min_y)), 1, (r, g, b))
+        cv.Line(i, (int(curr_pt.x-min_x), int(curr_pt.y-min_y)), (int(pt.x-min_x), int(pt.y-min_y)), (r, g, b))
+        curr_pt = pt
     cv.ShowImage("a_window",i)
     cv.WaitKey()
 
@@ -176,7 +234,16 @@ class ActiveShapeModel:
     self.shapes = shapes
     # Make sure the shape list is valid
     self.__check_shapes(shapes)
-    self.weights = self.__create_weight_matrix(shapes)
+    # Create weight matrix for points
+    print "Calculating weight matrix"
+    self.w = self.__create_weight_matrix(shapes)
+    # Align all shapes
+    print "Aligning shapes with Procrustes analysis"
+    self.shapes = self.__procrustes(shapes)
+    print "Producing model"
+    # Initialise this in constructor
+    self.num_modes = 0
+    self.__produce_model(self.shapes)
 
   def __check_shapes(self, shapes):
     """ Method to check that all shapes have the correct number of
@@ -187,13 +254,65 @@ class ActiveShapeModel:
         if shape.num_pts != num_pts:
           raise Exception("Shape has incorrect number of points")
 
+  def __get_mean_shape(self, shapes):
+    s = shapes[0]
+    for shape in shapes[1:]:
+      s = s + shape
+    return s / len(shapes)
+
+  def __produce_model(self, shapes):
+    """ Actually produces the shape model
+    """
+    shape_vectors = np.array([s.get_vector() for s in self.shapes])
+    mean = np.mean(shape_vectors, axis=0)
+    # Produce covariance matrix
+    cov = np.cov(shape_vectors, rowvar=0)
+    # Find eigenvalues/vectors of the covariance matrix
+    evals, evecs = np.linalg.eig(cov)
+
+    print evals
+    print evecs[0]
+
+    for m in range(-1000,1000):
+      test = mean + m*evecs[:,0]
+      s = Shape([])
+      # Make a shape
+      for i,j in np.reshape(test, (-1,2)):
+        s.add_point(Point(i, j))
+      # Show the shape
+      ShapeViewer.show_shapes([s])
+
+
   def __procrustes(self, shapes):
     """ This function aligns all shapes passed as a parameter by using
     Procrustes analysis
 
     :param shapes: A list of Shape objects
     """
-    pass
+    # First rotate/scale/translate each shape to match first in set
+    shapes[1:] = [s.align_to_shape(shapes[0], self.w) for s in shapes[1:]]
+
+    # Keep hold of a shape to align to each iteration to allow convergence
+    a = shapes[0]
+    trans = np.zeros((4, len(shapes)))
+    converged = False
+    while not converged:
+      # Now get mean shape
+      mean = self.__get_mean_shape(shapes)
+      # Align to shape to stop it diverging
+      mean = mean.align_to_shape(a, self.w)
+      # Now align all shapes to the mean
+      for i in range(len(shapes)):
+        # Get transformation required for each shape
+        trans[:, i] = shapes[i].get_alignment_params(mean, self.w)
+        # Apply the transformation
+        shapes[i] = shapes[i].apply_params_to_shape(trans[:,i])
+
+      # Test if the average transformation required is very close to the
+      # identity transformation and stop iteration if it is
+      if np.mean(np.array([1, 0, 0, 0]) - np.mean(trans, axis=1))**2 < 1e-25:
+        converged = True
+    return shapes
 
   def __create_weight_matrix(self, shapes):
     """ Private method to produce the weight matrix which corresponds
@@ -230,11 +349,12 @@ class ActiveShapeModel:
 def main():
   shapes = PointsReader.read_directory(sys.argv[1])
   a = ActiveShapeModel(shapes)
-  new = shapes[0].align_to_shape(shapes[1], a.weights)
+  #new = shapes[0].align_to_shape(shapes[1], a.w)
   #ShapeViewer.show_shapes([new, shapes[0]])
   #ShapeViewer.show_shapes([new, shapes[1]])
-  ShapeViewer.show_shapes([new, shapes[0], shapes[1]])
-
+  #ShapeViewer.show_shapes([new, shapes[0], shapes[1]])
+  #ShapeViewer.show_shapes(shapes)
+  #ShapeViewer.show_shapes(s)
   print "Finished"
 
 if __name__ == "__main__":
